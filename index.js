45d420ed9485c1451564df085ce06091e53b37b4
mariaclarinha27 <gokujosiana@gmail.com>
2024-08-12 23:10:19 +0000

    12/08/2024

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const idade = 28;
+let idade = 28;
 const anoNascimento = 1995;
 let anoAtual = 2024;
+const nomeCompleto = "Lucas Stasiak";
+
+console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual " +anoAtual + " completo " + idade + " anos");
 
 anoAtual = anoAtual + 1;
-
 console.log(anoAtual);
+
+idade = idade + 1;
+console.log(idade);
