daf7501cb3f332d3bb94e0a7ebe61368d32c5b79
mariaclarinha27 <gokujosiana@gmail.com>
2024-08-26 23:04:39 +0000

    26/08/24

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ console.log(anoAtual);
 
 idade = idade + 1;
 console.log(idade);
+
+
+//Agência de viagens Hawaii Paris Nova York Rio de Janeiro Tóquio 
+const listaDeDestinos = new Array(
+  "Hawaii",
+  "Paris",
+  "Nova York",
+  "Rio de Janeiro",
+  "Tóquio"
+);
+
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[3]);
+listaDeDestinos.push("Coronel Vivida");
+listaDeDestinos.push("Curitiba");
+listaDeDestinos.push("Madrid");
+listaDeDestinos.push("Berlin");
+console.log(listaDeDestinos);
